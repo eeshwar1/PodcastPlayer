fc74b37c80a21e23d16c8dbabc4a69b53d472dd8
Venky Venkatakrishnan <venkyul@gmail.com>
2018-11-03 10:39:11 -0500

    Initial Commit

---
+++ PodcastPlayer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PodcastPlayer
+//
+//  Created by Venky Venkatakrishnan on 11/3/18.
+//  Copyright © 2018 Venky Venkatakrishnan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
